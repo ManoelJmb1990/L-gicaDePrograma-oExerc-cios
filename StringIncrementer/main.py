891ef5361b6df4d176dbe23b88640d835c974491
Manoel Jorge <manoel.jmb1990@gmail.com>
2026-02-12 08:41:29 -0300

    Feito if/else para testes

--- StringIncrementer/main.py
+++ StringIncrementer/main.py
@@ -1,22 +1,38 @@
-"""A sua função é criar um programa que leia string e incremente para
-criar novas strings.
-
-number = 0
-se string[-1] == int:
-    number += string[-1]
-    insert(-1, number)
-
-
-    vamos tentar resolver com re (Expressões Regulares)"""
-
 import re
 
-entrada = "foobar23bla50"
+string = "foobar99"
 
-valor_numerico = re.search(r'\d+$', entrada).group()
-valor_numerico = int(valor_numerico) + 1
+if len(string) > 0 and re.search(r"\d+", string):
 
-entrada = re.search(r'[a-zA-Z]+', entrada).group()
+    string = string[::-1]  # Utilizei uma técnica chamada Slicing para inverter a string
+    numeric_value = re.search(r'\d+', string).group()
+    # Através da expressão regular
+    # extraí o primeiro evento numérico
 
-wordCombination = entrada + str(valor_numerico)
-print(wordCombination)
+    stringSize = len(numeric_value)
+    # peguei o tamanho da string numeric_value para usar como argumento no zfill
+
+    string = string[len(numeric_value):]
+    # Essa também é uma forma de Slicing.
+    # This too is a form of slicing
+    string = string[::-1]
+
+    numeric_value = int(numeric_value[::-1]) + 1  # coloquei os números na ordem novamente
+    numeric_value = str(numeric_value)
+    numeric_value = numeric_value[::-1].zfill(stringSize) #conferir a necessidade de inverter aqui
+    # Completei com zeros usando o argumento o tamanho original do int
+
+    string = string + numeric_value
+    print(string)
+elif len(string) > 0:
+    numeric_value = "1"
+    string += numeric_value
+    print(string)
+
+else:
+    numeric_value = 1
+    print(numeric_value)
+
+
+
+
